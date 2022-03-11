c9dfd5567b681766b3e676b474e00996bd940034
lixianle <1208884260@qq.com>
2022-03-11 20:42:36 +0800

    project init

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p class="home">
+      Home
+    </p>
+    <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .home
+    font-size: 50px
+    color: red
+</style>
